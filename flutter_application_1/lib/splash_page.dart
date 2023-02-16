077de8cf6ce1e2cfd7c7270c5c0f55cc0e9c0da7
Siriphun Chojorhor <palm.181433@gmail.com>
2023-02-16 13:09:55 +0700

    ีimplement code to login main and add splash page

---
+++ flutter_application_1/lib/splash_page.dart
@@ -0,0 +1,46 @@
+import "dart:async";
+
+import "package:flutter/material.dart";
+import "package:flutter_application_1/login.dart";
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        Duration(seconds: 2),
+        () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => login())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          color: Color.fromARGB(255, 63, 81, 181),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Image.asset(
+              'asset/icon_arshop.png',
+              height: 150.0,
+              width: 150.0,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
