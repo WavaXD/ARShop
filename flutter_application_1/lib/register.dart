3da6143f7646b004c503a19b5b1d1aa36c235d90
Siriphun Chojorhor <palm.181433@gmail.com>
2023-02-16 17:17:14 +0700

    create new page home and register

---
+++ flutter_application_1/lib/register.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class register extends StatelessWidget {
+  const register({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('register'),
+      color: Colors.amber,
+    );
+  }
+}
