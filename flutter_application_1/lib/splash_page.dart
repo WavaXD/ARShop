3f603ee2e9f369061d0a0a6267f2928b2b70265f
Siriphun Chojorhor <palm.181433@gmail.com>
2023-02-16 17:16:54 +0700

    change duration for splash page

--- flutter_application_1/lib/splash_page.dart
+++ flutter_application_1/lib/splash_page.dart
@@ -15,7 +15,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     Timer(
-        Duration(seconds: 2),
+        Duration(seconds: 3),
         () => Navigator.pushReplacement(
             context, MaterialPageRoute(builder: (context) => login())));
   }
