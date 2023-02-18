be789bcf743d76a9e631f7ffc559344e7dd0ae80
Wayu Saengphithak <wavagravity@gmail.com>
2023-02-18 14:48:05 +0700

    update test controller

--- ARShop/src/main/java/com/arshop/springboot/demo/ARShop/auth/AuthenticationController.java
+++ ARShop/src/main/java/com/arshop/springboot/demo/ARShop/auth/AuthenticationController.java
@@ -1,11 +1,10 @@
 package com.arshop.springboot.demo.ARShop.auth;
 
+import com.arshop.springboot.demo.ARShop.config.JwtService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -13,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
 
     private final AuthenticationService service;
+    private JwtService jwtService;
 
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(
@@ -26,5 +26,21 @@ public class AuthenticationController {
             @RequestBody AuthenticationRequest request
     ){
         return ResponseEntity.ok(service.authenticate(request));
+    }
+
+    @GetMapping("/extract")
+    public String extractJwt(HttpServletRequest request){
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+
+        jwt = authHeader.substring(7);
+
+        return extractEmail(jwt);
+    }
+
+    public String extractEmail(String jwt){
+        final String userEmail;
+        userEmail = jwtService.extractUsername(jwt);
+        return userEmail;
     }
 }
--- ARShop/src/main/java/com/arshop/springboot/demo/ARShop/rest/TestController.java
+++ ARShop/src/main/java/com/arshop/springboot/demo/ARShop/rest/TestController.java
@@ -1,9 +1,17 @@
 package com.arshop.springboot.demo.ARShop.rest;
 
+import com.arshop.springboot.demo.ARShop.auth.AuthenticationController;
+import com.arshop.springboot.demo.ARShop.config.JwtService;
 import com.arshop.springboot.demo.ARShop.entity.Customer;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.Date;
 
 @RestController
@@ -11,9 +19,22 @@ import java.util.Date;
 public class TestController {
 
     private Customer theCustomer;
+    private AuthenticationController authenticationController;
 
     @GetMapping("/hello")
     public String testHello(){
         return "hello";
     }
+
+    @GetMapping("/extract")
+    public String extractJwt(HttpServletRequest request){
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+
+        jwt = authHeader.substring(7);
+        authenticationController.extractEmail(jwt);
+
+        return "extract test";
+    }
+
 }
