89d09b39f67f5041e9532b55bdde6798d323a810
Wayu Saengphithak <wavagravity@gmail.com>
2023-03-08 23:49:51 +0700

    add find by product id

--- ARShop/src/main/java/com/arshop/springboot/demo/ARShop/dao/ProductPictureRepository.java
+++ ARShop/src/main/java/com/arshop/springboot/demo/ARShop/dao/ProductPictureRepository.java
@@ -3,5 +3,9 @@ package com.arshop.springboot.demo.ARShop.dao;
 import com.arshop.springboot.demo.ARShop.entity.ProductPicture;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductPictureRepository extends JpaRepository<ProductPicture,Integer> {
+
+    List<ProductPicture> findByProductID(int productID);
 }
