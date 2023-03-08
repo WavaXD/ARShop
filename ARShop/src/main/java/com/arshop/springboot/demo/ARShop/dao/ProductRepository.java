b9425030b270755550d0579bdebb5bde8d2b0ec6
Wayu Saengphithak <wavagravity@gmail.com>
2023-03-08 23:50:31 +0700

    add find by product name with recommendation query

--- ARShop/src/main/java/com/arshop/springboot/demo/ARShop/dao/ProductRepository.java
+++ ARShop/src/main/java/com/arshop/springboot/demo/ARShop/dao/ProductRepository.java
@@ -2,6 +2,15 @@ package com.arshop.springboot.demo.ARShop.dao;
 
 import com.arshop.springboot.demo.ARShop.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
 
 public interface ProductRepository extends JpaRepository<Product,Integer> {
+
+    @Query("Select c from Product c Inner join ProductScore s ON c.productID = s.productID where c.productName like %:productName% ORDER BY s.watchedTime DESC ")
+    List<Product> findByProductName(String productName);
+
+    //@Query("Select c from Product c where c.productName like %:productName%")
 }
