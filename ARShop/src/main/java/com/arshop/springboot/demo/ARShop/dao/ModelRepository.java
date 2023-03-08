70870b851077c8d1423058cf083b80d1602056c1
Wayu Saengphithak <wavagravity@gmail.com>
2023-03-08 23:49:41 +0700

    add find by productID

--- ARShop/src/main/java/com/arshop/springboot/demo/ARShop/dao/ModelRepository.java
+++ ARShop/src/main/java/com/arshop/springboot/demo/ARShop/dao/ModelRepository.java
@@ -3,5 +3,9 @@ package com.arshop.springboot.demo.ARShop.dao;
 import com.arshop.springboot.demo.ARShop.entity.Model;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ModelRepository extends JpaRepository<Model,Integer> {
+
+    List<Model> findByProductID(int productID);
 }
