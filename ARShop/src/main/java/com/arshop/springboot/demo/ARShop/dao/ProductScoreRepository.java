3b1174a799132bce99157839245de111365b3119
Wayu Saengphithak <wavagravity@gmail.com>
2023-03-08 23:50:47 +0700

    add find by customerID and productID

--- ARShop/src/main/java/com/arshop/springboot/demo/ARShop/dao/ProductScoreRepository.java
+++ ARShop/src/main/java/com/arshop/springboot/demo/ARShop/dao/ProductScoreRepository.java
@@ -3,5 +3,8 @@ package com.arshop.springboot.demo.ARShop.dao;
 import com.arshop.springboot.demo.ARShop.entity.ProductScore;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductScoreRepository extends JpaRepository<ProductScore,Integer> {
+    List<ProductScore> findByCustomerIDAndProductID(int customerID , int productID);
 }
