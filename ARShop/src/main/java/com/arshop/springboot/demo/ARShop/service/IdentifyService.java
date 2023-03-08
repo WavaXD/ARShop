a6a3b4a0719d77afeedb79dcded87745ec3a0ea6
Wayu Saengphithak <wavagravity@gmail.com>
2023-03-08 23:54:16 +0700

    implement to indentify jwt to get customerID

---
+++ ARShop/src/main/java/com/arshop/springboot/demo/ARShop/service/IdentifyService.java
@@ -0,0 +1,43 @@
+package com.arshop.springboot.demo.ARShop.service;
+
+import com.arshop.springboot.demo.ARShop.dao.CustomerRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.Base64;
+
+@Service
+public class IdentifyService {
+
+    private final CustomerRepository repository;
+
+    public IdentifyService(CustomerRepository repository) {
+        this.repository = repository;
+    }
+
+    public Integer extractJwt(HttpServletRequest request){
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+
+        jwt = authHeader.substring(7);
+
+        String[] chunks = jwt.split("\\.");
+
+        Base64.Decoder decoder = Base64.getUrlDecoder();
+        //String header = new String(decoder.decode(chunks[0]));
+        String payload = new String(decoder.decode(chunks[1]));
+
+        String[] splitPayload = payload.split("(?!\\.)(?!\\@)\\W");
+
+        System.out.println(splitPayload[5]);
+
+        String jwtEmail = splitPayload[5];
+
+        var user = repository.findByCustomerEmail(jwtEmail)
+                .orElseThrow();
+
+        System.out.println(jwtEmail);
+
+        return user.getCustomerID();
+    }
+}
