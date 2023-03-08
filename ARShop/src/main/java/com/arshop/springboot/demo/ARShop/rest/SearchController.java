27fa918a9b42aa25a20e75ef60eb3353b375b906
Wayu Saengphithak <wavagravity@gmail.com>
2023-03-08 23:52:44 +0700

    implement search and get a single product detail controller

--- ARShop/src/main/java/com/arshop/springboot/demo/ARShop/rest/SearchController.java
+++ ARShop/src/main/java/com/arshop/springboot/demo/ARShop/rest/SearchController.java
@@ -1,11 +1,27 @@
 package com.arshop.springboot.demo.ARShop.rest;
 
+import com.arshop.springboot.demo.ARShop.entity.Product;
 import com.arshop.springboot.demo.ARShop.service.SearchService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1")
 public class SearchController {
     @Autowired
     private SearchService searchService;
+
+    @PostMapping("/search")
+    public List<Product> contentSearch(@RequestBody Product keyword , HttpServletRequest request){
+        return searchService.contentSearch(keyword, request);
+    }
+
+    @PostMapping("/product")
+    public List getProduct(@RequestBody Product productID , HttpServletRequest request){
+        return searchService.productDetail(productID , request);
+    }
+
 }
