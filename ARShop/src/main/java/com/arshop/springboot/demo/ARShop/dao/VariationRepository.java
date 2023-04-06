c0f555f1c0f298b8345e996b3d27535a80665b27
Wayu Saengphithak <wavagravity@gmail.com>
2023-04-06 13:36:32 +0700

    Update VariationRepository.java

--- ARShop/src/main/java/com/arshop/springboot/demo/ARShop/dao/VariationRepository.java
+++ ARShop/src/main/java/com/arshop/springboot/demo/ARShop/dao/VariationRepository.java
@@ -3,5 +3,9 @@ package com.arshop.springboot.demo.ARShop.dao;
 import com.arshop.springboot.demo.ARShop.entity.Variation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface VariationRepository extends JpaRepository<Variation,Integer> {
+
+    List<Variation> findByProductID(int productID);
 }
