869bc2de47f1ad985a7be57930e458898587d022
Wayu Saengphithak <wavagravity@gmail.com>
2023-04-06 13:36:29 +0700

    Update OrderDetailRepository.java

--- ARShop/src/main/java/com/arshop/springboot/demo/ARShop/dao/OrderDetailRepository.java
+++ ARShop/src/main/java/com/arshop/springboot/demo/ARShop/dao/OrderDetailRepository.java
@@ -3,6 +3,9 @@ package com.arshop.springboot.demo.ARShop.dao;
 import com.arshop.springboot.demo.ARShop.entity.OrderDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderDetailRepository extends JpaRepository<OrderDetail,Integer> {
+import java.util.List;
+import java.util.Optional;
 
+public interface OrderDetailRepository extends JpaRepository<OrderDetail,Integer> {
+    List<OrderDetail> findByProductIDAndVariationIDAndCartID(int productID,int variationID,int cartID);
 }
