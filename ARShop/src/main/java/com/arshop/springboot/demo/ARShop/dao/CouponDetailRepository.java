6e80a0d58ce7db0036af195126c67d21be121c4f
Wayu Saengphithak <wavagravity@gmail.com>
2023-04-14 21:15:30 +0700

    Update CouponDetailRepository.java

--- ARShop/src/main/java/com/arshop/springboot/demo/ARShop/dao/CouponDetailRepository.java
+++ ARShop/src/main/java/com/arshop/springboot/demo/ARShop/dao/CouponDetailRepository.java
@@ -4,4 +4,5 @@ import com.arshop.springboot.demo.ARShop.entity.CouponDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CouponDetailRepository extends JpaRepository<CouponDetail,Integer> {
+
 }
