ab18549ce441ae026edf6eb7fc48b463fe1662ee
Wayu Saengphithak <wavagravity@gmail.com>
2023-04-14 21:16:02 +0700

    Update CartController.java

--- ARShop/src/main/java/com/arshop/springboot/demo/ARShop/rest/CartController.java
+++ ARShop/src/main/java/com/arshop/springboot/demo/ARShop/rest/CartController.java
@@ -1,24 +1,31 @@
 package com.arshop.springboot.demo.ARShop.rest;
 
 import com.arshop.springboot.demo.ARShop.entity.*;
+import com.arshop.springboot.demo.ARShop.service.CartService;
 import com.arshop.springboot.demo.ARShop.service.ProductDetailService;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/cart")
 public class CartController {
+    private final CartService cartService;
     private final ProductDetailService productDetailService;
 
-    public CartController(ProductDetailService productDetailService) {
+    public CartController(CartService cartService, ProductDetailService productDetailService) {
+        this.cartService = cartService;
         this.productDetailService = productDetailService;
     }
 
     @PostMapping("/add")
     public void addToCart(HttpServletRequest request , @RequestBody OrderDetail productDetail){
         productDetailService.addToCart(request,productDetail);
+    }
+
+    @GetMapping("/request")
+    public List<OrderDetail> getItems(HttpServletRequest request){
+        return cartService.getItems(request);
     }
 }
