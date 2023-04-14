855301aada3457571e769983d2700ca3f6e9c8fb
Wayu Saengphithak <wavagravity@gmail.com>
2023-04-14 21:16:07 +0700

    Update CartService.java

--- ARShop/src/main/java/com/arshop/springboot/demo/ARShop/service/CartService.java
+++ ARShop/src/main/java/com/arshop/springboot/demo/ARShop/service/CartService.java
@@ -1,16 +1,25 @@
 package com.arshop.springboot.demo.ARShop.service;
 
 import com.arshop.springboot.demo.ARShop.dao.CartRepository;
+import com.arshop.springboot.demo.ARShop.dao.OrderDetailRepository;
 import com.arshop.springboot.demo.ARShop.entity.Cart;
+import com.arshop.springboot.demo.ARShop.entity.OrderDetail;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CartService {
 
     private final CartRepository cartRepository;
+    private final OrderDetailRepository orderDetailRepository;
+    private final IdentifyService identifyService;
 
-    public CartService(CartRepository cartRepository) {
+    public CartService(CartRepository cartRepository, OrderDetailRepository orderDetailRepository, IdentifyService identifyService) {
         this.cartRepository = cartRepository;
+        this.orderDetailRepository = orderDetailRepository;
+        this.identifyService = identifyService;
     }
 
     public void initializeCart(int customerID){
@@ -25,6 +34,12 @@ public class CartService {
         var cart = cartRepository.findByCustomerID(customerID);
 
         return cart.get(0).getCartID();
+    }
+
+    public List<OrderDetail> getItems(HttpServletRequest request){
+        var items = orderDetailRepository.findByCartID(getCartID(identifyService.extractJwt(request)));
+
+        return items;
     }
 
 }
