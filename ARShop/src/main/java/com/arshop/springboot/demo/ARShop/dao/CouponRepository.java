75964c560a304e99ce600741f23d6a220e3fa5ca
Wayu Saengphithak <wavagravity@gmail.com>
2023-04-14 21:15:41 +0700

    Update CouponRepository.java

--- ARShop/src/main/java/com/arshop/springboot/demo/ARShop/dao/CouponRepository.java
+++ ARShop/src/main/java/com/arshop/springboot/demo/ARShop/dao/CouponRepository.java
@@ -2,6 +2,12 @@ package com.arshop.springboot.demo.ARShop.dao;
 
 import com.arshop.springboot.demo.ARShop.entity.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CouponRepository extends JpaRepository<Coupon,Integer> {
+
+    @Query("Select c From Coupon c Where c.couponID = (Select b.couponID From CouponDetail b Where b.customerID = :customerID) AND c.vendorID = :vendorID")
+    List<Coupon> findAvailableCoupon(int customerID, int vendorID);
 }
