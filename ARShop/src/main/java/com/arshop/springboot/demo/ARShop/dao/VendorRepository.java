8d00e1f7a2b36b0c951a71473c1079c01374c26d
Wayu Saengphithak <wavagravity@gmail.com>
2023-04-14 21:15:53 +0700

    Update VendorRepository.java

--- ARShop/src/main/java/com/arshop/springboot/demo/ARShop/dao/VendorRepository.java
+++ ARShop/src/main/java/com/arshop/springboot/demo/ARShop/dao/VendorRepository.java
@@ -4,4 +4,5 @@ import com.arshop.springboot.demo.ARShop.entity.Vendor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VendorRepository extends JpaRepository<Vendor,Integer> {
+    Vendor findByVendorID(int vendorID);
 }
