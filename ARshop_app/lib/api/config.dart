92a9e38bf92ae5f2c879c300659f3d48731d7918
Palm <palm.181433@gmail.com>
2023-04-20 12:40:28 +0700

    app path APIs

--- ARshop_app/lib/api/config.dart
+++ ARshop_app/lib/api/config.dart
@@ -8,4 +8,13 @@ class Config_api {
   static const String productDetailAPI = "/api/v1/product";
   static const String poppularProductAPI = "/api/v1/popular";
   static const String recommendProductAPI = "/api/v1/recommend";
+  static const String addProductToCartAPI = "/api/v1/cart/add";
+  static const String deleteProductInCartAPI = "/api/v1/cart/add";
+  static const String showProductInCartAPI = "/api/v1/cart/request";
+  static const String getCouponMacthVendorAPI = "/api/v1/coupon/available";
+  static const String getCouponofSpecificVendorAPI = "/api/v1/coupon/available";
+  static const String collectCouponAPI = "api/v1/coupon/2";
+  static const String processOrderAPI = "api/v1/order/process";
+  static const String getOrderAPI = "api/v1/order/orders";
+  static const String getOrderDetailAPI = "/api/v1/order/2";
 }
